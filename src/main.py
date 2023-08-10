8fde3ab526028ea299494e08892900ee587e98d6
liusa <liusa@bupt>
2023-08-10 08:05:50 +0800

    feat: start from zero

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 import leetcode_view
 import codeforces_view
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
 
 def main():
     leetcode_view.process()
     codeforces_view.process()
-    print(1);
 
 if __name__ == "__main__":
     main()
